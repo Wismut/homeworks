8f870fe249e86dffa85e7be39580877fe4dfdd65
Sergiy <asd@sed.dfs>
2020-05-09 06:50:29 +0300

    Refactoring

--- src/main/java/multithreading/Task02/Runner.java
+++ src/main/java/multithreading/Task02/Runner.java
@@ -5,55 +5,54 @@ import java.util.function.IntConsumer;
 public class Runner {
     public static void main(String[] args) {
         FizzBuzz fizzBuzz = new FizzBuzz(15);
-        Runnable fizzRunnable = new Runnable() {
-            @Override
-            public void run() {
-                try {
-                    fizzBuzz.fizz(this);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
+        Runnable fizzRunnable = () -> System.out.print("fizz ");
+        Runnable buzzRunnable = () -> System.out.print("buzz ");
+        Runnable fizzBuzzRunnable = () -> System.out.print("fizzbuzz ");
+        IntConsumer numberConsumer = n -> System.out.print(n + " ");;
+        Thread threadA = new Thread(() -> {
+            try {
+                fizzBuzz.fizz(fizzRunnable);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
             }
-        };
-        Runnable buzzRunnable = new Runnable() {
-            @Override
-            public void run() {
-                try {
-                    fizzBuzz.buzz(this);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
+        });
+        Thread threadB = new Thread(() -> {
+            try {
+                fizzBuzz.buzz(buzzRunnable);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
             }
-        };
-        Runnable fizzBuzzRunnable = new Runnable() {
-            @Override
-            public void run() {
-                try {
-                    fizzBuzz.fizzbuzz(this);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
+        });
+        Thread threadC = new Thread(() -> {
+            try {
+                fizzBuzz.fizzbuzz(fizzBuzzRunnable);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
             }
-        };
-        IntConsumer numberConsumer = new IntConsumer() {
-            @Override
-            public void accept(int value) {
-                try {
-                    fizzBuzz.number(this);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-            }
-        };
-//        Thread threadA = new Thread(() -> {
-//
-//        });
-//        Thread threadB = new Thread(fizzBuzz::buzz);
-//        Thread threadC = new Thread(fizzBuzz::fizzBuzz);
-//        Thread threadD = new Thread(fizzBuzz::number);
-//        threadA.start();
-//        threadB.start();
-//        threadC.start();
-//        threadD.start();
+        });
+        Thread threadD = new Thread(new PrintNumberRunnable(fizzBuzz, numberConsumer));
+        threadA.start();
+        threadB.start();
+        threadC.start();
+        threadD.start();
+    }
+}
+
+class PrintNumberRunnable implements Runnable {
+    FizzBuzz fizzBuzz;
+    IntConsumer consumer;
+
+    public PrintNumberRunnable(FizzBuzz fizzBuzz, IntConsumer consumer) {
+        this.fizzBuzz = fizzBuzz;
+        this.consumer = consumer;
+    }
+
+    @Override
+    public void run() {
+        try {
+            fizzBuzz.number(consumer);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
     }
 }
