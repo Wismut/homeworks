540edcd09f3ae000ba29bac17d1971994a0529e1
Sergiy <asd@sed.dfs>
2020-05-09 14:34:48 +0300

    Add CountDownLatch

--- src/main/java/multithreading/Task02/Runner.java
+++ src/main/java/multithreading/Task02/Runner.java
@@ -1,5 +1,6 @@
 package multithreading.Task02;
 
+import java.util.concurrent.CountDownLatch;
 import java.util.function.IntConsumer;
 
 public class Runner {
@@ -11,39 +12,42 @@ public class Runner {
         Runnable buzzRunnable = () -> System.out.print("buzz ");
         Runnable fizzBuzzRunnable = () -> System.out.print("fizzbuzz ");
         IntConsumer numberConsumer = n -> System.out.print(n + " ");
-        Thread threadA = new Thread(new FizzRunnable(fizzBuzz, fizzRunnable));
+        CountDownLatch countDownLatch = new CountDownLatch(1);
+        Thread threadA = new Thread(new FizzRunnable(fizzBuzz, fizzRunnable, countDownLatch));
         threadA.setDaemon(true);
         threadA.start();
-        Thread threadB = new Thread(new BuzzRunnable(fizzBuzz, buzzRunnable));
+        Thread threadB = new Thread(new BuzzRunnable(fizzBuzz, buzzRunnable, countDownLatch));
         threadB.setDaemon(true);
         threadB.start();
-        Thread threadC = new Thread(new FizzBuzzRunnable(fizzBuzz, fizzBuzzRunnable));
+        Thread threadC = new Thread(new FizzBuzzRunnable(fizzBuzz, fizzBuzzRunnable, countDownLatch));
         threadC.setDaemon(true);
         threadC.start();
-        Thread threadD = new Thread(new PrintNumberRunnable(fizzBuzz, numberConsumer));
+        Thread threadD = new Thread(new PrintNumberRunnable(fizzBuzz, numberConsumer, countDownLatch));
         threadD.setDaemon(true);
         threadD.start();
-        executingTasks(fizzBuzz, threadA, threadB, threadC, threadD);
+        executingTasks(fizzBuzz, countDownLatch, threadA, threadB, threadC, threadD);
     }
 
     private static void executingTasks(FizzBuzz fizzBuzz,
+                                       CountDownLatch countDownLatch,
                                        Thread threadA,
                                        Thread threadB,
                                        Thread threadC,
                                        Thread threadD) throws InterruptedException {
         for (i = 1; i <= fizzBuzz.getN(); i++) {
+            countDownLatch = new CountDownLatch(1);
             if (i % 15 == 0) {
                 threadC.interrupt();
-                Thread.sleep(200);
+                countDownLatch.await();
             } else if (i % 3 == 0) {
                 threadA.interrupt();
-                Thread.sleep(200);
+                countDownLatch.await();
             } else if (i % 5 == 0) {
                 threadB.interrupt();
-                Thread.sleep(200);
+                countDownLatch.await();
             } else {
                 threadD.interrupt();
-                Thread.sleep(200);
+                countDownLatch.await();
             }
         }
     }
@@ -52,10 +56,12 @@ public class Runner {
 class FizzRunnable implements Runnable {
     FizzBuzz fizzBuzz;
     Runnable fizzRunnable;
+    CountDownLatch countDownLatch;
 
-    public FizzRunnable(FizzBuzz fizzBuzz, Runnable fizzRunnable) {
+    public FizzRunnable(FizzBuzz fizzBuzz, Runnable fizzRunnable, CountDownLatch countDownLatch) {
         this.fizzBuzz = fizzBuzz;
         this.fizzRunnable = fizzRunnable;
+        this.countDownLatch = countDownLatch;
     }
 
     @Override
@@ -68,6 +74,8 @@ class FizzRunnable implements Runnable {
                     fizzBuzz.fizz(fizzRunnable);
                 } catch (InterruptedException e1) {
 
+                } finally {
+                    countDownLatch.countDown();
                 }
             }
         }
@@ -75,12 +83,14 @@ class FizzRunnable implements Runnable {
 }
 
 class BuzzRunnable implements Runnable {
+    private final CountDownLatch countDownLatch;
     FizzBuzz fizzBuzz;
     Runnable buzzRunnable;
 
-    public BuzzRunnable(FizzBuzz fizzBuzz, Runnable buzzRunnable) {
+    public BuzzRunnable(FizzBuzz fizzBuzz, Runnable buzzRunnable, CountDownLatch countDownLatch) {
         this.fizzBuzz = fizzBuzz;
         this.buzzRunnable = buzzRunnable;
+        this.countDownLatch = countDownLatch;
     }
 
     @Override
@@ -93,6 +103,8 @@ class BuzzRunnable implements Runnable {
                     fizzBuzz.buzz(buzzRunnable);
                 } catch (InterruptedException e1) {
 
+                } finally {
+                    countDownLatch.countDown();
                 }
             }
         }
@@ -100,12 +112,14 @@ class BuzzRunnable implements Runnable {
 }
 
 class FizzBuzzRunnable implements Runnable {
+    private final CountDownLatch countDownLatch;
     FizzBuzz fizzBuzz;
     Runnable fizzBuzzRunnable;
 
-    public FizzBuzzRunnable(FizzBuzz fizzBuzz, Runnable fizzBuzzRunnable) {
+    public FizzBuzzRunnable(FizzBuzz fizzBuzz, Runnable fizzBuzzRunnable, CountDownLatch countDownLatch) {
         this.fizzBuzz = fizzBuzz;
         this.fizzBuzzRunnable = fizzBuzzRunnable;
+        this.countDownLatch = countDownLatch;
     }
 
     @Override
@@ -118,6 +132,8 @@ class FizzBuzzRunnable implements Runnable {
                     fizzBuzz.fizzbuzz(fizzBuzzRunnable);
                 } catch (InterruptedException e1) {
 
+                } finally {
+                    countDownLatch.countDown();
                 }
             }
         }
@@ -125,12 +141,14 @@ class FizzBuzzRunnable implements Runnable {
 }
 
 class PrintNumberRunnable implements Runnable {
+    private final CountDownLatch countDownLatch;
     FizzBuzz fizzBuzz;
     IntConsumer consumer;
 
-    public PrintNumberRunnable(FizzBuzz fizzBuzz, IntConsumer consumer) {
+    public PrintNumberRunnable(FizzBuzz fizzBuzz, IntConsumer consumer, CountDownLatch countDownLatch) {
         this.fizzBuzz = fizzBuzz;
         this.consumer = consumer;
+        this.countDownLatch = countDownLatch;
     }
 
     @Override
@@ -143,6 +161,8 @@ class PrintNumberRunnable implements Runnable {
                     fizzBuzz.number(consumer);
                 } catch (InterruptedException e1) {
 
+                } finally {
+                    countDownLatch.countDown();
                 }
             }
         }
